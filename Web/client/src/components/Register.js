c6c3fdabf1a1b257c830b2bf3d68be94a472f258
Prajjwal Kumar <prajjwalkr1999@gmail.com>
2020-10-26 23:06:29 +0530

    fake

--- Web/client/src/components/Register.js
+++ Web/client/src/components/Register.js
@@ -125,7 +125,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
--- Web/client/src/components/Signin.js
+++ Web/client/src/components/Signin.js
@@ -102,7 +102,7 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
