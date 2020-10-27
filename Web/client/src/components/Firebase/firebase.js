2d188d9f0be310396e08a9b03e9b0ff0820eebe0
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-10-28 00:42:57 +0530

    firestore implememted

--- Web/client/src/components/Firebase/firebase.js
+++ Web/client/src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,6 +15,7 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
 
   // *** Auth API ***
