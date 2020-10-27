09d926abcfaaef35298797b25b619fc1570cfc6c
Prajjwal Kumar <prajjwalkr1999@gmail.com>
2020-10-27 18:09:04 +0530

    Forgot/change feature added

---
+++ Web/client/src/pages/passchange.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Passwordchange from "../components/Passwordchange"
+
+export default class passchange extends Component {
+    render() {
+        return (
+            <div>
+                <Passwordchange />
+            </div>
+        )
+    }
+}
+
